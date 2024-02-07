a91f03bcb530709d14fbf1a63f8958a6bd3c36cb
Joël Henri <ebosqua.edimo@gmail.com>
2024-02-07 23:03:42 +0100

    Alternative

--- PDFgenerator.js
+++ PDFgenerator.js
@@ -1,7 +1,5 @@
 const express = require("express")
 
-// const file = require("./file.pdf")
-
 const fs = require("fs")
 
 const PDFDocument = require('pdfkit');
@@ -23,15 +21,16 @@ app.get("", (req, res) => {
   
     const doc = new PDFDocument({ bufferPages: true, font: 'Courier' });
 
-    doc.pipe(fs.createWriteStream('output.pdf'));
+    // doc.pipe(fs.createWriteStream('output.pdf'));
     
     // const stream = doc.pipe(blobStream());
     // doc.pipe(fs.createWriteStream('./file.pdf'))
     // const PDF = require("./file.pdf")
-    // doc.pipe(res)
+    // Attacher le PDF directement à la réponse dès la fin de la création du document
+    doc.pipe(res)
         
-    doc.on("data", (data) => {res.write(data)})
-    doc.on("end", () => {res.end()})
+    // doc.on("data", (data) => {res.write(data)})
+    // doc.on("end", () => {res.end()})
 
     doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
 
@@ -67,28 +66,34 @@ app.get("", (req, res) => {
         ellipsis: true
     });
 
-    res.writeHead(200,{"Content-Type": "application/pdf", "Content-Disposition": `attachment;filename=output.pdf`})
+    // res.writeHead(200,{"Content-Type": "application/pdf", "Content-Disposition": `attachment;filename=output.pdf`})
     
     doc.end();
     // res.header("Content-Type", "application/pdf").send(doc)
 
     // stream.on('finish', () => {
-      // get a blob you can do whatever you like with
-      // const blob = stream.toBlob('application/pdf');
+    //   // get a blob you can do whatever you like with
+    //   const blob = stream.toBlob('application/pdf');
+    //   // console.log(blob)
+    //   // const url = URL.createObjectURL(blob)
+    //   // console.log(url)
     
-      // or get a blob URL for display in the browser
-      // const url = stream.toBlobURL('application/pdf');
-    //   iframe.src = url;
-      // console.log(url)
-      // console.log(doc)
-    // res.header("Content-Type", "application/pdf").send(url)${url}
-    // res.header("Content-Type", "application/pdf").header(`Content-Disposition", "attachment;filename=hehe.pdf`).send('Here is some vector graphics...').end()
-    // res.header("Content-Type", "text/html").header("Content-Disposition", "attachment;filname=hehe.pdf").send(`<iframe src="blob:http://127.0.0.1:3250/00fc1af2-268a-4af7-a010-4db27b9fcecd" width="500px" height="500px"></iframe>`)
-    // res.header("Content-Type", "text/html").send(`<iframe src="${url}" width="500px" height="500px"></iframe>`)
-    // res.header("Content-Type", "application/pdf").send(PDF)
-    //   res.header("Content-Type", "application/pdf").send(`<script > window.open(${url})</script>`)
-    //   res.header("Content-Type", "text/html").send(`<iframe src="http://localhost:3250/${url}" width="200px"></iframe>`)
-    //   res.header("Content-Type", "text/html").send(`<embed type="application/pdf" original-url="${url}">`)
+    //   res.header("Content-Type", "application/pdf").send(blob)
+
+      
+    //   // or get a blob URL for display in the browser
+    //   // const url = stream.toBlobURL('application/pdf');
+    // //   iframe.src = url;
+    //   // console.log(url)
+    //   // console.log(doc)
+    // // res.header("Content-Type", "application/pdf").send(url)${url}
+    // // res.header("Content-Type", "application/pdf").header(`Content-Disposition", "attachment;filename=hehe.pdf`).send('Here is some vector graphics...').end()
+    // // res.header("Content-Type", "text/html").header("Content-Disposition", "attachment;filname=hehe.pdf").send(`<iframe src="blob:http://127.0.0.1:3250/00fc1af2-268a-4af7-a010-4db27b9fcecd" width="500px" height="500px"></iframe>`)
+    // // res.header("Content-Type", "text/html").send(`<iframe src="${url}" width="500px" height="500px"></iframe>`)
+    // // res.header("Content-Type", "application/pdf").send(PDF)
+    // //   res.header("Content-Type", "application/pdf").send(`<script > window.open(${url})</script>`)
+    // //   res.header("Content-Type", "text/html").send(`<iframe src="http://localhost:3250/${url}" width="200px"></iframe>`)
+    // //   res.header("Content-Type", "text/html").send(`<embed type="application/pdf" original-url="${url}">`)
     // });
 })
 
