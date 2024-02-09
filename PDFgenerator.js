d0f39cc7f08624995d20f6eaccb9c501162bcb0e
Joël Henri <ebosqua.edimo@gmail.com>
2024-02-09 21:01:52 +0100

    Début Devis et polices

--- PDFgenerator.js
+++ PDFgenerator.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const { createPDF } = require("./functions")
 
-const { addSample } = require("./functions")
+// const { addSample } = require("./functions")
 
 // const fs = require("fs")
 
@@ -29,93 +29,31 @@ app.get("/", (req, res) => {
   }
 
   const resWriteHead = () => {
-    res.writeHead(200,{"Content-Disposition": `attachment;filename=output.pdf`})
-
+    res.writeHead(200, {"Content-Disposition": `attachment;filename=output.pdf`})
   }
 
   createPDF(false, res, resWrite, resEnd, "addSample", resWriteHead)
-//     const doc = new PDFDocument({ bufferPages: true, font: 'Courier' });
 
-//     // doc.pipe(fs.createWriteStream('output.pdf'));
-    
-//     // const stream = doc.pipe(blobStream());
-//     // doc.pipe(fs.createWriteStream('./file.pdf'))
-//     // const PDF = require("./file.pdf")
-//     // Attacher le PDF directement à la réponse dès la fin de la création du document
-//     // doc.pipe(res)
-        
-//     doc.on("data", (data) => {res.write(data)})
-//     doc.on("end", () => {res.end()})
-
-//     doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
-
-// // some vector graphics
-//     doc
-//     .save()
-//     .moveTo(100, 150)
-//     .lineTo(100, 250)
-//     .lineTo(200, 250)
-//     .fill('#FF3300');
-
-//     doc.circle(280, 200, 50).fill('#6600FF');
-
-//     // an SVG path
-//     doc
-//     .scale(0.6)
-//     .translate(470, 130)
-//     .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-//     .fill('red', 'even-odd')
-//     .restore();
-
-//     // and some justified text wrapped into columns
-//     doc
-//     .text('here is some wrapped text...', 100, 300)
-//     .font('Times-Roman', 13)
-//     .moveDown()
-//     .text("lorem", {
-//         width: 412,
-//         align: 'justify',
-//         indent: 30,
-//         columns: 2,
-//         height: 30,
-//         ellipsis: true
-//     });
-
-//     res.writeHead(200,{"Content-Type": "application/pdf", "Content-Disposition": `attachment;filename=output.pdf`})
-    
-//     doc.end();
-//     // res.header("Content-Type", "application/pdf").send(doc)
-
-//     // stream.on('finish', () => {
-//     //   // get a blob you can do whatever you like with
-//     //   const blob = stream.toBlob('application/pdf');
-//     //   // console.log(blob)
-//     //   // const url = URL.createObjectURL(blob)
-//     //   // console.log(url)
-    
-//     //   res.header("Content-Type", "application/pdf").send(blob)
-
-      
-//     //   // or get a blob URL for display in the browser
-//     //   // const url = stream.toBlobURL('application/pdf');
-//     // //   iframe.src = url;
-//     //   // console.log(url)
-//     //   // console.log(doc)
-//     // // res.header("Content-Type", "application/pdf").send(url)${url}
-//     // // res.header("Content-Type", "application/pdf").header(`Content-Disposition", "attachment;filename=hehe.pdf`).send('Here is some vector graphics...').end()
-//     // // res.header("Content-Type", "text/html").header("Content-Disposition", "attachment;filname=hehe.pdf").send(`<iframe src="blob:http://127.0.0.1:3250/00fc1af2-268a-4af7-a010-4db27b9fcecd" width="500px" height="500px"></iframe>`)
-//     // // res.header("Content-Type", "text/html").send(`<iframe src="${url}" width="500px" height="500px"></iframe>`)
-//     // // res.header("Content-Type", "application/pdf").send(PDF)
-//     // //   res.header("Content-Type", "application/pdf").send(`<script > window.open(${url})</script>`)
-//     // //   res.header("Content-Type", "text/html").send(`<iframe src="http://localhost:3250/${url}" width="200px"></iframe>`)
-//     // //   res.header("Content-Type", "text/html").send(`<embed type="application/pdf" original-url="${url}">`)
-//     // });
 })
 
-// app.get("/getfile", (req,res) => {
-//   console.log(__dirname + "\\file.pdf")
-//   res.header("Content-Type", "application/pdf").sendFile(__dirname + "\\output.pdf")
-// })
+app.get("/getfile", (req,res) => {
+
+  const resWrite = (data) => {
+    res.write(data)
+  }
+
+  const resEnd = () => {
+    res.end()
+  }
+
+  const resWriteHead = () => {
+    res.writeHead(200, {"Content-Disposition": `attachment;filename=output.pdf`})
+  }
+
+  // createPDF(true, res, resWrite, resEnd, "addSample", resWriteHead)
+  createPDF(true, res, resWrite, resEnd, "createQuotation", resWriteHead)
+
+})
 
 
 
--- functions.js
+++ functions.js
@@ -40,8 +40,93 @@ function addSample(document){
     });
 }
 
+function createQuotation(document, number){
+
+    // doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+    // document.
+    document
+    .font("./Fonts/TT Chocolates Trial Bold.otf")
+    .fillColor("#11EBCD")
+    .fontSize(45)
+    .text(`DEVIS N° ${number}`)
+    // document.fontSize(40).font("./Fonts/couture-bld.otf").text(`DEVIS N${number}`, 300, 85);
+
+    // document
+    // .moveDown(1)
+
+    document
+    .moveTo(51,120)
+    .lineTo(560, 120)
+    .lineWidth(4)
+    .fillAndStroke("#11EBCD", "#11EBCD")
+
+    let height = document.fontSize(10).heightOfString(`Bonjour \n Bonsoir \n Bonne après-midi \n Bonne nuit`, {
+        width: 250,
+        align: "left"})
+    
+    console.log(height)
+
+    document
+    .moveDown(4)
+    .fontSize(10)
+    .fillColor("#000000",0.65)
+    .text(`Baptiste Gauthier \n17 Avenue \n60356 Austanville\n06 84 73 94 85 \n Baptiste Gauthier \n17 Avenue \n60356 Austanville\n06 84 73 94 85`, {
+        columns: 2,
+        columnGap: 10,
+        height: height,
+        width: 750,
+        align: "left",
+        // continued: true,
+        // width: 150
+    })
+    
+    // document.list(["lorem ipsum lorem ipsum lorem ipsum", "lorem ipsumum"], 100, 200, "bulletRadius")
+
+    document.fill("#FFFF00")
+
+    // // some vector graphics
+    // document
+    // .save()
+    // .moveTo(100, 150)
+    // .lineTo(100, 250)
+    // .lineTo(200, 250)
+    // .fill('#FF3300');
+
+    // document.circle(280, 200, 50).fill('#6600FF');
+
+    // // an SVG path
+    // document
+    // .scale(0.6)
+    // .translate(470, 130)
+    // .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+    // .fill('red', 'even-odd')
+    // .restore();
+
+    // // and some justified text wrapped into columns
+    // document
+    // .text('And here is some wrapped text...', 100, 300)
+    // .font('Times-Roman', 13)
+    // .moveDown()
+    // .text("lorem", {
+    //     width: 412,
+    //     align: 'justify',
+    //     indent: 30,
+    //     columns: 2,
+    //     height: 300,
+    //     ellipsis: true
+    // });
+}
+
+
+// #14B3C5
+
 function createPDF(visualize, response, onData, onEnd, whichFunction, addHeaders){
-    const doc = new PDFDocument({ bufferPages: true, font: 'Courier' })
+    const doc = new PDFDocument({ 
+        bufferPages: true,
+        font: 'Courier-Bold',
+        margin: 50
+      })
+
     
     if(visualize){
         doc.pipe(response)
@@ -52,8 +137,14 @@ function createPDF(visualize, response, onData, onEnd, whichFunction, addHeaders
         doc.on("end", onEnd)
     }
 
+    // doc.fill
+
     if(whichFunction === "addSample"){
         addSample(doc)
+    }
+
+    if(whichFunction === "createQuotation"){
+        createQuotation(doc,20)
     }
 
     if(!visualize){
